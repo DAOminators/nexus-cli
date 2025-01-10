0acb9bea839a38c59a2ea36827ffaa0d6bb30af6
ankitprasad2005 <ankitprasad2005@outlook.com>
2025-01-10 16:41:00 +0530

    slight change

--- contracts/MangoRepo.sol
+++ contracts/MangoRepo.sol
@@ -1,126 +1,96 @@
-/*
- * Mango Repository
- * Copyright (C) 2016 Alex Beregszaszi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License only.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "github.com/axic/mango/MangoRepoInterface.sol";
 
 contract MangoRepo is MangoRepoInterface {
     bool obsolete;
-    mapping (address => bool) admins;
-    mapping (address => bool) committers;
+    mapping(address => bool) admins;
+    mapping(address => bool) committers;
     string[] refKeys;
-    mapping (string => string) refs;
+    mapping(string => string) refs;
     string[] snapshots;
 
     modifier committerOnly {
-        if (!committers[msg.sender]) {
-            throw;
-        }
-
-        _
+        require(committers[msg.sender], "Committer only");
+        _;
     }
 
     modifier adminOnly {
-        if (!admins[msg.sender]) {
-            throw;
-        }
-
-        _
+        require(admins[msg.sender], "Admin only");
+        _;
     }
 
-    function MangoRepo() {
+    constructor() {
         admins[msg.sender] = true;
         committers[msg.sender] = true;
     }
 
-    function repoInterfaceVersion() constant returns (uint version) {
+    function repoInterfaceVersion() external pure override returns (uint version) {
         version = 1;
     }
 
-    function refCount() constant returns (uint) {
+    function refCount() external view override returns (uint) {
         return refKeys.length;
     }
 
-    function refName(uint index) constant returns (string ref) {
+    function refName(uint index) external view override returns (string memory ref) {
         ref = refKeys[index];
     }
 
-    function getRef(string ref) constant returns (string hash) {
+    function getRef(string memory ref) external view override returns (string memory hash) {
         hash = refs[ref];
     }
 
-    function __findRef(string ref) private returns (int) {
-        /* Horrible way to add a new key to the list */
-
-        for (var i = 0; i < refKeys.length; i++)
-            if (strEqual(refKeys[i], ref))
-                return i;
-
+    function __findRef(string memory ref) private view returns (int) {
+        for (uint i = 0; i < refKeys.length; i++)
+            if (keccak256(abi.encodePacked(refKeys[i])) == keccak256(abi.encodePacked(ref)))
+                return int(i);
         return -1;
     }
-    function setRef(string ref, string hash) committerOnly {
+
+    function setRef(string memory ref, string memory hash) external override committerOnly {
         if (__findRef(ref) == -1)
             refKeys.push(ref);
-
         refs[ref] = hash;
     }
 
-    function deleteRef(string ref) committerOnly {
+    function deleteRef(string memory ref) external override committerOnly {
         int pos = __findRef(ref);
         if (pos != -1) {
-            // FIXME: shrink the array?
             refKeys[uint(pos)] = "";
         }
-
-        // FIXME: null? string(0)?
         refs[ref] = "";
     }
 
-    function strEqual(string a, string b) private returns (bool) {
-        return sha3(a) == sha3(b);
-    }
-
-    function snapshotCount() constant returns (uint) {
+    function snapshotCount() external view override returns (uint) {
         return snapshots.length;
     }
 
-    function getSnapshot(uint index) constant returns (string) {
+    function getSnapshot(uint index) external view override returns (string memory) {
         return snapshots[index];
     }
 
-    function addSnapshot(string hash) committerOnly {
+    function addSnapshot(string memory hash) external override committerOnly {
         snapshots.push(hash);
     }
 
-    function setObsolete() adminOnly {
+    function setObsolete() external adminOnly {
         obsolete = true;
     }
 
-    function isObsolete() constant returns (bool) {
+    function isObsolete() external view override returns (bool) {
         return obsolete;
     }
 
-    function authorize(address addr, bool admin) adminOnly {
+    function authorize(address addr, bool admin) external adminOnly {
         committers[addr] = true;
         if (admin) {
             admins[addr] = true;
         }
     }
 
-    function deauthorize(address addr, bool admin) adminOnly {
+    function deauthorize(address addr, bool admin) external adminOnly {
         if (admin) {
             admins[addr] = false;
         } else {
